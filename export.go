b8fdcfcada45051ac34bf083f9598d94d0d1d780
vipally <vipally@gmail.com>
2017-10-16 16:08:22 +0800

    doc

--- export.go
+++ export.go
@@ -25,7 +25,7 @@
 //
 // Support all serialize-able data types:
 //
-//	  bool, int8, int16, int32, int64,
+//    bool, int8, int16, int32, int64,
 //    uint8, uint16, uint32, uint64,
 //    float32, float64, complex64, complex128,
 //    string, struct, slice, array, map.
