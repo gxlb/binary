f4d07fd1e792db86a4b88d129c0e11d8502f960b
Ally Dale <vipally@gmail.com>
2017-10-22 21:16:44 +0800

    message

--- encoder.go
+++ encoder.go
@@ -190,7 +190,7 @@ func (this *Encoder) Value(x interface{}) (err error) {
 
 	if p, ok := x.(Packer); ok {
 		if _, _ok := x.(Sizer); !_ok { //interface verification
-			panic(fmt.Errorf("pected but not Sizer: %s", v.Type().String()))
+			panic(fmt.Errorf("expect but not Sizer: %s", v.Type().String()))
 		}
 
 		r, err := p.Pack(this.buff[this.pos:])
