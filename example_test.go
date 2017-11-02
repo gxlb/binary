cf456299d0aac39f253a6c5d7ba4bec413149e77
vipally <vipally@gmail.com>
2017-11-02 12:06:38 +0800

    examples

--- example_test.go
+++ example_test.go
@@ -23,6 +23,7 @@ func ExampleWrite() {
 		fmt.Println("binary.Write failed:", err)
 	}
 	fmt.Printf("%#v", buf.Bytes())
+
 	// Output:
 	// []byte{0x18, 0x2d, 0x44, 0x54, 0xfb, 0x21, 0x9, 0x40}
 }
@@ -41,6 +42,7 @@ func ExampleWrite_multi() {
 		}
 	}
 	fmt.Printf("%#v", buf.Bytes())
+
 	// Output:
 	// []byte{0xbe, 0xef, 0xca, 0xfe}
 }
@@ -54,43 +56,48 @@ func ExampleRead() {
 		fmt.Println("binary.Read failed:", err)
 	}
 	fmt.Print(pi)
+
 	// Output:
 	// 3.141592653589793
 }
 
 func ExampleEncode() {
-	var s struct {
+	var data struct {
 		A uint32
 		B int
 		C string
 	}
-	s.A = 0x11223344
-	s.B = -5
-	s.C = "hello"
-	b, err := binary.Encode(s, nil)
+	data.A = 0x11223344
+	data.B = -5
+	data.C = "hello"
+
+	b, err := binary.Encode(data, nil)
 	if err != nil {
 		fmt.Println("binary.Encode failed:", err)
 	}
 	fmt.Printf("%#v", b)
+
 	// Output:
 	// []byte{0x44, 0x33, 0x22, 0x11, 0x9, 0x5, 0x68, 0x65, 0x6c, 0x6c, 0x6f}
 }
 func ExampleEncode_withbuffer() {
-	var s struct {
+	var data struct {
 		A uint32
 		B int
 		C string
 	}
-	s.A = 0x11223344
-	s.B = -5
-	s.C = "hello"
-	size := binary.Sizeof(s)
+	data.A = 0x11223344
+	data.B = -5
+	data.C = "hello"
+	size := binary.Sizeof(data)
 	buffer := make([]byte, size)
-	b, err := binary.Encode(s, buffer)
+
+	b, err := binary.Encode(data, buffer)
 	if err != nil {
 		fmt.Println("binary.Encode failed:", err)
 	}
 	fmt.Printf("%#v", b)
+
 	// Output:
 	// []byte{0x44, 0x33, 0x22, 0x11, 0x9, 0x5, 0x68, 0x65, 0x6c, 0x6c, 0x6f}
 }
@@ -105,18 +112,19 @@ func ExampleEncode_bools() {
 		G bool    //true
 		H uint8   //0x33
 	}
-	var bools = boolset{
+	var data = boolset{
 		0x11, true, 0x22, []bool{true, false, true}, true, nil, true, 0x33,
 	}
-	b, err := binary.Encode(bools, nil)
+	b, err := binary.Encode(data, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
-	size := binary.Sizeof(bools)
-	if size != len(b) {
+
+	if size := binary.Sizeof(data); size != len(b) {
 		fmt.Printf("Encode got %#v %+v\nneed %+v\n", len(b), b, size)
 	}
-	fmt.Printf("Encode %#v\nsize=%d result=%#v", bools, len(b), b)
+	fmt.Printf("Encode %#v\nsize=%d result=%#v", data, len(b), b)
+
 	// Output:
 	// Encode binary_test.boolset{A:0x11, B:true, C:0x22, D:[]bool{true, false, true}, E:true, F:(*uint32)(nil), G:true, H:0x33}
 	// size=6 result=[]byte{0x11, 0xb, 0x22, 0x3, 0x5, 0x33}
@@ -139,21 +147,25 @@ func ExampleDecode() {
 }
 func ExampleEncoder() {
 	encoder := binary.NewEncoder(100)
+
 	encoder.Uint32(0x11223344, false)
 	encoder.Varint(-5)
 	encoder.String("hello")
 	encodeResult := encoder.Buffer()
 	fmt.Printf("%#v", encodeResult)
+
 	// Output:
 	// []byte{0x44, 0x33, 0x22, 0x11, 0x9, 0x5, 0x68, 0x65, 0x6c, 0x6c, 0x6f}
 }
 func ExampleDecoder() {
 	buffer := []byte{0x44, 0x33, 0x22, 0x11, 0x9, 0x5, 0x68, 0x65, 0x6c, 0x6c, 0x6f}
+
 	decoder := binary.NewDecoder(buffer)
 	u32 := decoder.Uint32(false)
 	i, _ := decoder.Varint()
 	str := decoder.String()
 	fmt.Printf("%#v %#v %#v", u32, i, str)
+
 	// Output:
 	// 0x11223344 -5 "hello"
 }
@@ -200,14 +212,14 @@ func ExampleBinarySerializer() {
 		func (this *S) Encode() ([]byte, error) {
 			encoder := binary.NewEncoder(this.Size())
 			encoder.Value(this.A)
-			encoder.Int16(int16(this.B))
+			encoder.Int16(int16(this.B), false)
 			encoder.Value(this.C)
 			return encoder.Buffer(), nil
 		}
 		func (this *S) Decode(buffer []byte) error {
 			decoder := binary.NewDecoder(buffer)
 			decoder.Value(&this.A)
-			this.B = int(decoder.Int16())
+			this.B = int(decoder.Int16(false))
 			decoder.Value(&this.C)
 			return nil
 		}
@@ -216,20 +228,22 @@ func ExampleBinarySerializer() {
 	s.A = 0x11223344
 	s.B = -5
 	s.C = "hello"
-	b, err := binary.Encode(&s, nil)
 
+	b, err := binary.Encode(&s, nil)
 	if err != nil {
 		fmt.Println("binary.Encode failed:", err)
 	}
+
 	err = binary.Decode(b, &ss)
 	if err != nil {
 		fmt.Println("binary.Decode failed:", err)
 	}
-	fmt.Printf("[%+v\n%#v\n%+v]", s, b, ss)
+	fmt.Printf("%+v\n%#v\n%+v", s, b, ss)
+
 	// Output:
-	// [{A:287454020 B:-5 C:hello}
+	// {A:287454020 B:-5 C:hello}
 	// []byte{0x44, 0x33, 0x22, 0x11, 0xfb, 0xff, 0x5, 0x68, 0x65, 0x6c, 0x6c, 0x6f}
-	// {A:287454020 B:-5 C:hello}]
+	// {A:287454020 B:-5 C:hello}
 }
 
 func ExampleSizeof() {
@@ -261,6 +275,7 @@ func ExampleSizeof() {
 	size := binary.Sizeof(s)
 
 	fmt.Printf("Sizeof(s)  = %d\nstd.Size(s)= %d\ngob.Size(s)= %d", size, stdSize, gobSize)
+
 	// Output:
 	// Sizeof(s)  = 133
 	// std.Size(s)= 217
@@ -275,6 +290,7 @@ func ExampleRegStruct() {
 		D uint
 	}
 	binary.RegStruct((*someRegedStruct)(nil))
+
 	// Output:
 }
 
@@ -300,9 +316,10 @@ func ExampleRegStruct_packedInts() {
 	if size := binary.Sizeof(ints); size != len(b) {
 		fmt.Printf("PackedInts got %+v %+v\nneed %+v\n", len(b), b, size)
 	}
-	fmt.Printf("Encode %+v\nsize=%d reault=%#v\n", ints, len(b), b)
+
+	fmt.Printf("Encode %+v\nsize=%d result=%#v", ints, len(b), b)
 
 	// Output:
 	// Encode {A:1 B:2 C:3 D:4 E:5 F:6 G:[7 8 9] H:10}
-	// size=10 reault=[]byte{0x2, 0x4, 0x6, 0x4, 0x5, 0x6, 0x3, 0x7, 0x8, 0x9}
+	// size=10 result=[]byte{0x2, 0x4, 0x6, 0x4, 0x5, 0x6, 0x3, 0x7, 0x8, 0x9}
 }
