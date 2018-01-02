1911b2208314ac538892c1b09abbb26b2ebb59d4
Ally Dale <vipally@gmail.com>
2018-01-02 18:42:33 +0800

    build error

--- random/random_test.go
+++ random/random_test.go
@@ -1,7 +1,6 @@
 package random
 
 import (
-	"fmt"
 	//"fmt"
 	"testing"
 )
@@ -18,32 +17,32 @@ type all struct {
 
 func TestRand(t *testing.T) {
 	Default.Srand(RandSeed(0))
-	//	//	var cnt [256]int
-	//	//	total := 6553600
-	//	//	for i := 0; i < total; i++ {
-	//	//		r := rand.Uint16()
-	//	//		if r < 256 {
-	//	//			cnt[r]++
-	//	//		}
-
-	//	//	}
-	//	//	for i, v := range cnt {
-	//	//		fmt.Printf("%3d %4d/%4d\n", i, v, total/65536)
-	//	//	}
-	//	println(rand.String(12))
-	//	println(rand.Float64())
-	//	println(rand.Complex128())
-	//	println(rand.Uint32())
-	//	println(rand.Uint64())
-	//	println(rand.Float32())
-	//	for i := 0; i < 50; i++ {
-	//		for j := 0; j < 8; j++ {
-	//			print(rand.Uint8(), "\t")
+	//	var cnt [256]int
+	//	total := 6553600
+	//	for i := 0; i < total; i++ {
+	//		r := Default.Uint16()
+	//		if r < 256 {
+	//			cnt[r]++
 	//		}
-	//		println("")
+
 	//	}
-	var v all
-	Default.Value(&v)
-	fmt.Printf("%@v\n", v)
+	//	for i, v := range cnt {
+	//		fmt.Printf("%3d %4d/%4d\n", i, v, total/65536)
+	//	}
+	println(Default.String(12))
+	println(Default.Float64())
+	println(Default.Complex128())
+	println(Default.Uint32())
+	println(Default.Uint64())
+	println(Default.Float32())
+	for i := 0; i < 50; i++ {
+		for j := 0; j < 8; j++ {
+			print(Default.Uint8(), "\t")
+		}
+		println("")
+	}
+	//	var v all
+	//	Default.Value(&v)
+	//	fmt.Printf("%@v\n", v)
 
 }
